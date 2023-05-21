cdde8008eb6339da6585e23b9ff00e5baf17c2d0
jbarkle <jbarkle4@kent.edu>
2023-05-21 09:52:17 -0700

    begin coding flask app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,49 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
+import pandas as pd
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
-
-# Create our session (link) from Python to the DB
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
