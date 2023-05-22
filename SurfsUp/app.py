f4b287939c5fc27989e95872450c1f45d169f991
jbarkle <jbarkle4@kent.edu>
2023-05-21 17:03:57 -0700

    code routes /precipitation, /stations in flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -5,6 +5,7 @@ import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import numpy as np
 
 
 #################################################
@@ -44,6 +45,46 @@ def welcome():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
+# create a new route: /precipitation
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """Return a list of precipitation data including the date"""
+    # Query precipitation data
+    results = session.query(Measurement.date,Measurement.prcp).\
+    filter(Measurement.date <= '2017-08-23').filter(Measurement.date >= '2016-08-23').\
+    all()
+    session.close()
+    # Create a dictionary from the row data and append to a list of precip_data
+    precip_data = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["precipitation"] = prcp
+        precip_data.append(prcp_dict)
+    return jsonify(precip_data)
+
+# create a new route: /stations
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """Return a list of stations data"""
+    # Query stations data
+    stations_results = session.query(Measurement.station).\
+            group_by(Measurement.station).\
+            all()
+    session.close()
+    all_stations = list(np.ravel(stations_results))
+    return jsonify(all_stations)
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
